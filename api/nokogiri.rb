0f0932fd372a9192c6b1c1215883ebd8c146d7ee
Daniel Kehoe <daniel@danielkehoe.com>
2020-05-30 20:05:56 +0800

    wip: verify changes

--- api/nokogiri.rb
+++ api/nokogiri.rb
@@ -6,13 +6,13 @@ require 'open-uri'
 Handler = Proc.new do |req, res|
     # variables
     project = 'my_new_website'
-    title = 'My New Website'
+    title = 'Deployed on Vercel'
     # parse and replace
     uri = URI('https://raw.githubusercontent.com/yaxdotcom/yax-template-wip/master/index.html')
     page = Nokogiri::HTML(URI.open(uri))
     page.title = title
     # output
     res.status = 200
-    res['Content-Type'] = 'text/html'
+    res['Content-Type'] = 'text/plain'
     res.body = page.to_html
 end
