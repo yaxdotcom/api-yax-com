b0ff836a9a98cb379db271b539237e9559458a31
Daniel Kehoe <daniel@danielkehoe.com>
2020-05-31 15:17:57 +0800

    read template, create repo, write file

--- api/nokogiri.rb
+++ api/nokogiri.rb
@@ -7,35 +7,31 @@ require 'open-uri'
 Handler = Proc.new do |req, res|
  
     log = Logger.new(STDOUT)
-    log.info("\n" + 'ENV: ' +  ENV['OAUTH_TOKEN'] + "\n")
 
     # variables
-    project = 'my_new_website'
-    title = 'Deployed on Vercel'
+    project = 'my_vercel_website'
+    title = 'My Website Deployed on Vercel'
     # parse and replace
     uri = URI('https://raw.githubusercontent.com/yaxdotcom/yax-template-wip/master/index.html')
     page = Nokogiri::HTML(URI.open(uri))
     page.title = title
 
-    # api = Github.new
-    # api.oauth_token = "TODO"
-    # begin
-    #     # create a repo
-    #     api.repos.create name: project,
-    #         description: 'Website for ' + project,
-    #         private: false,
-    #         has_issues: true
-    #     # save a template file
-    #     api.repos.contents.create 'DanielKehoe', project, 'index.html',
-    #         # content: page.to_html,
-    #         content: page.to_html
-    #         path: 'index.html',
-    #         message: 'create file from template'
-    # rescue Github::Error::GithubError => e
-    #     log.error('\n' + e.message)
-    # end
-
-
+    api = Github.new
+    api.oauth_token = ENV['OAUTH_TOKEN']
+    begin
+        # create a repo
+        api.repos.create name: project,
+            description: title,
+            private: false,
+            has_issues: true
+        # save a template file
+        api.repos.contents.create 'DanielKehoe', project, 'index.html',
+            content: page.to_html,
+            path: 'index.html',
+            message: 'create file from template'
+    rescue Github::Error::GithubError => e
+        log.error('nokogiri.rb') { "\n" + e.message = "\n" }
+    end
 
     # output
     res.status = 301
