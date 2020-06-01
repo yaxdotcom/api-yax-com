7a39f9ca4a18e238fe854dd45b280a29899d3557
Daniel Kehoe <daniel@danielkehoe.com>
2020-06-01 18:27:43 +0800

    diagnostics for 'deploy'

--- api/deploy.rb
+++ api/deploy.rb
@@ -15,36 +15,44 @@ Handler = Proc.new do |req, res|
     title = req.query['title']
     description = req.query['description']
 
-    log.info('deploy.rb') { "\n template" + template= "\n" }
-    log.info('deploy.rb') { "\n account" + account = "\n" }
-    log.info('deploy.rb') { "\n repository" + repository = "\n" }
-    log.info('deploy.rb') { "\n title" + title = "\n" }
-    log.info('deploy.rb') { "\n description" + description = "\n" }
+    log.info('deploy.rb') { "\n template: " + template= "\n" }
+    log.info('deploy.rb') { "\n account: " + account = "\n" }
+    log.info('deploy.rb') { "\n repository: " + repository = "\n" }
+    log.info('deploy.rb') { "\n title: " + title = "\n" }
+    log.info('deploy.rb') { "\n description: " + description = "\n" }
 
-    # parse and replace
-    uri = URI("https://raw.githubusercontent.com/yaxdotcom/#{template}/master/index.html")
-    page = Nokogiri::HTML(URI.open(uri))
-    page.title = title
+    log.info('deploy.rb') { "\n req.path: " + req.path = "\n" }
+    log.info('deploy.rb') { "\n req.body: " + req.body = "\n" }
+    req.each { |head| puts " \n#{head}:  #{req[head]} \n" }
+    req.each { |param| puts " \n#{param}:  #{req[param]} \n" }
 
-    api = Github.new
-    api.oauth_token = ENV['OAUTH_TOKEN']
-    begin
-        # create a repo
-        api.repos.create name: repository,
-            description: description,
-            private: false,
-            has_issues: true
-        # save a template file
-        api.repos.contents.create account, repository, 'index.html',
-            content: page.to_html,
-            path: 'index.html',
-            message: 'create file from template'
-    rescue Github::Error::GithubError => e
-        log.error('nokogiri.rb') { "\n" + e.message = "\n" }
-    end
+    # # parse and replace
+    # uri = URI("https://raw.githubusercontent.com/yaxdotcom/#{template}/master/index.html")
+    # page = Nokogiri::HTML(URI.open(uri))
+    # page.title = title
 
-    # output
-    res.status = 301
-    res['Location'] = "https://github.com/#{account}?tab=repositories"
-    res.body = ''
+    # api = Github.new
+    # api.oauth_token = ENV['OAUTH_TOKEN']
+    # begin
+    #     # create a repo
+    #     api.repos.create name: repository,
+    #         description: description,
+    #         private: false,
+    #         has_issues: true
+    #     # save a template file
+    #     api.repos.contents.create account, repository, 'index.html',
+    #         content: page.to_html,
+    #         path: 'index.html',
+    #         message: 'create file from template'
+    # rescue Github::Error::GithubError => e
+    #     log.error('nokogiri.rb') { "\n" + e.message = "\n" }
+    # end
+
+    # # output
+    # res.status = 301
+    # res['Location'] = "https://github.com/#{account}?tab=repositories"
+    # res.body = ''
+
+    res.status = 200
+    res.body = 'processed'
 end
