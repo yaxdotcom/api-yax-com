fce014a1eeb011e6c441d40eba9d1f817f0e761b
Daniel Kehoe <daniel@danielkehoe.com>
2020-06-04 18:00:26 +0800

    ready to try as GitHub callback URL

--- api/deploy.rb
+++ api/deploy.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
-require 'logger'
 require 'github_api'
+require 'json'
+require 'logger'
 require 'nokogiri'
 require 'open-uri'
 
@@ -8,12 +9,15 @@ Handler = Proc.new do |req, res|
  
     log = Logger.new(STDOUT)
 
+    session_code = req.query['code']
+    params = JSON.parse(Base64.decode64(req.query['state'])).with_indifferent_access
+
     # parameters
-    template = req.query['templateId']
-    account = req.query['github-account']
-    repository = req.query['repository']
-    title = req.query['title']
-    description = req.query['description']
+    template = params['templateId']
+    account = 'DanielKehoe'
+    repository = params['repository']
+    title = params['title']
+    description = params['description']
 
     log.info('deploy.rb') { "\n template: " + template + "\n" }
     log.info('deploy.rb') { "\n account: " + account + "\n" }
@@ -21,33 +25,33 @@ Handler = Proc.new do |req, res|
     log.info('deploy.rb') { "\n title: " + title + "\n" }
     log.info('deploy.rb') { "\n description: " + description + "\n" }
 
-    # parse and replace
-    uri = URI("https://raw.githubusercontent.com/yaxdotcom/#{template}/master/index.html")
-    page = Nokogiri::HTML(URI.open(uri))
-    page.title = title
+    # # parse and replace
+    # uri = URI("https://raw.githubusercontent.com/yaxdotcom/#{template}/master/index.html")
+    # page = Nokogiri::HTML(URI.open(uri))
+    # page.title = title
 
-    api = Github.new
-    api.oauth_token = ENV['OAUTH_TOKEN']
-    begin
-        # create a repo
-        api.repos.create name: repository,
-            description: description,
-            private: false,
-            has_issues: true
-        # save a template file
-        api.repos.contents.create account, repository, 'index.html',
-            content: page.to_html,
-            path: 'index.html',
-            message: 'create file from template'
-    rescue Github::Error::GithubError => e
-        log.error('nokogiri.rb') { "\n" + e.message = "\n" }
-    end
+    # api = Github.new
+    # api.oauth_token = ENV['OAUTH_TOKEN']
+    # begin
+    #     # create a repo
+    #     api.repos.create name: repository,
+    #         description: description,
+    #         private: false,
+    #         has_issues: true
+    #     # save a template file
+    #     api.repos.contents.create account, repository, 'index.html',
+    #         content: page.to_html,
+    #         path: 'index.html',
+    #         message: 'create file from template'
+    # rescue Github::Error::GithubError => e
+    #     log.error('nokogiri.rb') { "\n" + e.message = "\n" }
+    # end
 
-    # output
-    res.status = 301
-    res['Location'] = "https://github.com/#{account}?tab=repositories"
-    res.body = ''
+    # # output
+    # res.status = 301
+    # res['Location'] = "https://github.com/#{account}?tab=repositories"
+    # res.body = ''
 
-    # res.status = 200
-    # res.body = 'processed'
+    res.status = 200
+    res.body = 'processed'
 end
