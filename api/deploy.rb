cfc7ca56a81c0d28e9e80c6086d5ad36f037294e
Daniel Kehoe <daniel@danielkehoe.com>
2020-06-06 13:15:28 +0800

    wip: read yaml file - 10

--- api/deploy.rb
+++ api/deploy.rb
@@ -26,7 +26,7 @@ Handler = Proc.new do |req, res|
 
     # download and parse a configuration file
     uri_yaml = "https://raw.githubusercontent.com/yaxdotcom/#{template}/master/yax.yaml"
-    config = YAML.parse(URI.parse(uri_yaml).open.read)
+    config = YAML.parse(URI.parse(uri_yaml).open.read).to_ruby
     log.info('deploy.rb') { "\n config.class.name: " + config.class.name + "\n" }
     log.info('deploy.rb') { "\n config.inspect: " + config.inspect + "\n" }
     log.info('deploy.rb') { "\n config.yax_version: " + config.yax_version + "\n" }
