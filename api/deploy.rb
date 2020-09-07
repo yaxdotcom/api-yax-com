a4fc3eb6c502dfccc81223b51b566dd351c5239f
Daniel Kehoe <daniel@danielkehoe.com>
2020-09-07 16:34:16 +0800

    use yax-fauna gem to log deploys to FaunaDB

--- api/deploy.rb
+++ api/deploy.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'fauna'
 require 'github_api'
 require 'json'
 require 'logger'
@@ -9,6 +10,12 @@ require 'yaml'
 Handler = Proc.new do |req, res|
 
     log = Logger.new(STDOUT)
+    observer = Fauna::ClientLogger.logger { |log| logger.debug(log) }
+
+    # create a FaunaDB client
+    $fauna = Fauna::Client.new(
+        secret: ENV['FAUNA_SERVER_KEY'],
+        observer: observer)
 
     # parameters
     authorization_code = req.query['code']
@@ -136,6 +143,18 @@ Handler = Proc.new do |req, res|
                     path: filename,
                     message: commit_msg
             end
+        end
+
+        # log data to FaunaDB
+        $fauna.query do
+            create ref('classes/deploys'), data: {
+                user_login: user.login,
+                url: "https://github.com/#{user.login}/#{repository}",
+                template: template,
+                repository: repository,
+                title: title,
+                description: description
+                }
         end
 
         # output
--- api/try.rb
+++ api/try.rb
@@ -1,52 +1,15 @@
 require 'rubygems'
 require 'cowsay'
 require 'fauna'
-require 'github_api'
-require 'json'
-require 'logger'
 require 'nokogiri'
-require 'open-uri'
-require 'webrick'
-require 'yaml'
 
-log = Logger.new(STDOUT)
 puts("Cowsay gem version: " + Cowsay::VERSION)
-puts("WEBrick gem version: " + WEBrick::VERSION)
-puts("JSON gem version: " + JSON::VERSION)
 puts("Fauna gem version: " + Fauna::VERSION)
-puts("Faraday gem version: " + Faraday::VERSION)
+puts("Nokogiri gem version: " + Nokogiri::VERSION)
 
-#observer = Fauna::ClientLogger.logger { |log| logger.debug(log) }
-
-$fauna = Fauna::Client.new(secret: ENV['FAUNA_SERVER_KEY'])
-
-class Handler < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET(req, res)
-    name = req.query['name'] || 'world'
-
-    # log data to FaunaDB
-    $fauna.query do
-      create ref('classes/deploys'), data: {
-        user_login: name,
-        url: "https://github.com/dk/template",
-        template: 'template',
-        repository: 'repository',
-        title: 'title',
-        description: 'description'
-      }
-    end
-
-    res.status = 200
-    res['Content-Type'] = 'text/text; charset=utf-8'
-    res.body = Cowsay.say("Hello #{name}", 'cow')
-  end
+Handler = Proc.new do |req, res|
+  name = req.query['name'] || 'World'
+  res.status = 200
+  res['Content-Type'] = 'text/text; charset=utf-8'
+  res.body = Cowsay.say("Hello #{name}", 'cow')
 end
-
-server = WEBrick::HTTPServer.new(:Port => 8000)
-server.mount("/", Handler)
-
-['INT', 'TERM'].each {|signal|
-  trap(signal) {server.shutdown}
-}
-
-server.start()
