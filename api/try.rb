7b200e8d61bae92b072f0bca761764280c5862e6
Daniel Kehoe <daniel@danielkehoe.com>
2020-09-08 15:09:29 +0800

    wip: try httprb on Vercel

--- api/try.rb
+++ api/try.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'cowsay'
 require 'fauna'
+require 'http'
 require 'nokogiri'
 require 'uri'
-require 'net/http'
 
 puts("Cowsay gem version: " + Cowsay::VERSION)
 puts("Fauna gem version: " + Fauna::VERSION)
@@ -11,15 +11,11 @@ puts("Nokogiri gem version: " + Nokogiri::VERSION)
 
 Handler = Proc.new do |req, res|
 
-  url = URI("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
-  https = Net::HTTP.new(url.host, url.port);
-  https.use_ssl = true
-  request = Net::HTTP::Get.new(url)
-  response = https.request(request)
+  response = HTTP.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
 
   name = req.query['name'] || 'World'
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
   # res.body = Cowsay.say("Hello #{name}", 'cow')
-  res.body = response.read_body
+  res.body = response.to_s
 end
