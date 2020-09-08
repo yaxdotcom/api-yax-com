ff2c39c4a0a9b81c4cb231001d2cd229afd5532c
Daniel Kehoe <daniel@danielkehoe.com>
2020-09-08 15:46:22 +0800

    wip: try httprb with Sendinblue on Vercel

--- api/try.rb
+++ api/try.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'cowsay'
 require 'fauna'
 require 'http'
+require 'json'
 require 'nokogiri'
-require 'uri'
 
 puts("Cowsay gem version: " + Cowsay::VERSION)
 puts("Fauna gem version: " + Fauna::VERSION)
@@ -11,7 +11,27 @@ puts("Nokogiri gem version: " + Nokogiri::VERSION)
 
 Handler = Proc.new do |req, res|
 
-  response = HTTP.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
+  payload = JSON.parse('{
+    "sender":{
+        "name":"Yax",
+        "email":"support@yax.com"
+    },
+    "to":[
+        {
+          "email":"daniel@danielkehoe.com",
+          "name":"Daniel Kehoe"
+        }
+    ],
+    "subject":"test mail from Vercel",
+    "htmlContent":"<html><head></head><body><h1>Hello this is a test email from sib</h1></body></html>"
+  }')
+
+  response = HTTP.headers(
+    'accept': 'application/json',
+    'content-type': 'application/json',
+    'api-key': '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'
+  ).post("https://api.sendinblue.com/v3/smtp/email", :json => payload )
+
 
   name = req.query['name'] || 'World'
   res.status = 200
