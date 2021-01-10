f2e52622f2e3defb117d90b45279ce3f77d38655
Daniel Kehoe <daniel@danielkehoe.com>
2021-01-10 10:10:35 +0000

    regex to replace nonalphanumerics in the repository name

--- api/deploy.rb
+++ api/deploy.rb
@@ -22,7 +22,7 @@ Handler = Proc.new do |req, res|
     authorization_code = req.query['code']
     params = JSON.parse(Base64.decode64(req.query['state']))
     template = params['templateId']
-    repository = params['repository']
+    repository = params['repository'].gsub(/(\W)/, "-")
     title = params['title']
     description = params['description']
 
@@ -63,16 +63,17 @@ Handler = Proc.new do |req, res|
         doc_preamble = <<~DOC
         # Project: #{params['title']}
 
-        This is the GitHub repository for the project you named "#{params['repository']}", generated from a
-        website template at [yax.com](https://yax.com). With GitHub, storage is permanent (and free).
+        This is the GitHub repository for the project you named "#{params['repository']}", generated from the "#{params['template']}" website template at [yax.com](https://yax.com).
 
-        From here, deploy your website for free hosting. Just click a button to deploy your website to [Netlify](https://www.netlify.com/), [Vercel](https://vercel.com/), or [Render.com](https://render.com/). During the process, you will create a second repo for deployment. Name it "#{params['repository']}-deploy".
+        From here, deploy your website for free hosting. Just click a button to deploy your website to [Netlify](https://www.netlify.com/), [Vercel](https://vercel.com/), or [Render.com](https://render.com/). During the process, you will create a second repo for deployment. Name it what you like; I suggest "#{params['repository']}-deploy".
 
         [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/#{user.login}/#{params['repository']})
 
         [![Deploy to Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/#{user.login}/#{params['repository']})
 
         [![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)](https://render.com/deploy)
+        
+        Yax is new. Please send feedback to [support@yax.com](mailto:support@yax.com?subject=[GitHub]%20#{params['repository']})
         
         For more information:
         - [tutorials.yax.com](https://tutorials.yax.com/) for helpful tutorials
